f2f5dc26ed83b812e1884ab44051131935b4141d
Vinay2603 <91530880+Vinay2603@users.noreply.github.com>
2021-12-24 23:00:22 +0530

    add

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 import React from "react"
 
 function App() {
+  /////////////
 const [counter,setCounter] = React.useState(5)
 
 const  Increment = (value)=>{
@@ -12,8 +13,22 @@ const  Increment = (value)=>{
 const Double =()=>{
   setCounter(counter *2)
 }
-
-
+///////////////
+const [query, SetQuery] = React.useState("")
+const [tasks,setTasks] = React.useState([])
+ const handleChange = (e)=>{
+  // console.log(e.target.value)
+  const {value} = e.target
+  SetQuery(value)
+}
+const handleAdd =()=>{
+  const payload={
+    title: query,
+    status : false
+  }
+  const newTasks = [...tasks , payload]
+   setTasks(newTasks)
+}
   return (
     <div className="App">
        
@@ -22,6 +37,19 @@ const Double =()=>{
        <button onClick={()=>Increment(1)}>ADD</button>
        <button onClick={()=> Increment(-1)}>REMOVE</button>
        <button onClick={()=> Double()}>DOUBLE</button>
+       
+       <div>
+         <h1>task</h1>
+         <div>
+              <input value={query} onChange={handleChange} placeholder='Add somthing '></input>
+              <button onClick={handleAdd}>ADD</button>
+         </div>
+          <div>
+            {tasks.map((el)=>{
+               return <div> {el.title}</div>
+            })}
+          </div>
+       </div>
     </div>
   );
 }
